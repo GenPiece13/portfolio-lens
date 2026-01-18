0e207a0f56150b0b2e82697fa77cb733c1a20160
FebriansyahHusniAdiatma <febriansyahadiatma@gmail.com>
2026-01-19 00:16:38 +0700

    another fixed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,16 +9,15 @@ const config: Config = {
     theme: {
         extend: {
             colors: {
-                paper: "#F2F2F0",     // Off-white
-                charcoal: "#111111",  // Black text
-                digital: "#4D4DFF",   // Blue accent
-                code: "#1A1A1A",      // Dark bg
+                paper: "#F2F2F0",     // Warna dasar
+                charcoal: "#111111",  // Warna teks hitam pekat
+                digital: "#4D4DFF",   // Warna aksen biru/ungu
+                code: "#1A1A1A",      // Background hitam coding
             },
             fontFamily: {
                 display: ["var(--font-syne)"],
                 mono: ["var(--font-space)"],
             },
-            // Daftarkan easing custom di sini
             transitionTimingFunction: {
                 'editorial': 'cubic-bezier(0.76, 0, 0.24, 1)',
             }
